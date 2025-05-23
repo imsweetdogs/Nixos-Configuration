64a3e48e53cc7557b66c8408d2ea9aebf557742b
sweetdogs <git@sweetdogs.ru>
2025-05-23 08:45:02 +0300

    inputs

--- flake.nix
+++ flake.nix
@@ -1,3 +1,30 @@
 {
     description = "Sweetdogs Nixos system configuration";
+
+    inputs = {
+        flake-parts.url = "github:hercules-ci/flake-parts";
+
+        master.url = "github:nixos/nixpkgs/master";
+        unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+        stable.url = "github:nixos/nixpkgs/nixos-24.11";
+        nixpkgs.follows = "unstable";
+
+        disko = {
+            url = "github:nix-community/disko";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+
+        home-manager = {
+            url = "github:nix-community/home-manager";
+            inputs.nixpkgs.follows = "nixpkgs";
+        };
+    };
+
+    outputs = { self, flake-parts, ... }@inputs: flake-parts.lib.mkFlake { inherit inputs; } {
+        systems = inputs.nixpkgs.lib.systems.flakeExposed;
+
+        flake = {
+
+        };
+    };
 }
