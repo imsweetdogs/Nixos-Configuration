75b4ffc4cfdb1cb9e2bdbdcd611c2a0e6090adab
sweetdogs <git@sweetdogs.ru>
2025-05-27 21:13:54 +0300

    secrets

--- config/default.nix
+++ config/default.nix
@@ -1,10 +1,11 @@
 {
     structure = {
         overlays = ./overlays;
+        secrets = ./secrets;
     };
 
     system = {
-        basePassword = "$6$pokedim13$2HDvjLbVa6wItmJRywWvxO2dB2Wxopjvt3DY9CU3qMJc/8Ho6eoV8PWcUG/0M03avtMb1DYKQT63ZpYqPCUWL1";
+        basePassword = "$6$pokedim13$2HDvjLbVa6wItmJRywWvxO2dB2Wxopjvt3DY9CU3qMJc/8Ho6eoV8PWcUG/0M03avtMb1DYKQT63ZpYqPCUWL1"; # mkpasswd -m sha512crypt -S
         timeZone = "Europe/Moscow";
         stateVersion = "25.05";
         users = ["sweetdogs"];
