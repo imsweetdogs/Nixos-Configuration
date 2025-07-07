3f48ee9d2134b727aaa44f3e5d510ae34f7023b4
imsweetdogs <git@sweetdogs.ru>
2025-07-07 15:32:29 +0000

    config and overlays

---
+++ config/default.nix
@@ -0,0 +1,9 @@
+{
+  system = {
+    timeZone = "Europe/Moscow";
+    stateVersion = "25.05";
+    users = [ "sweetdogs" ];
+    baseHashedPassword = "$6$pokedim13$2HDvjLbVa6wItmJRywWvxO2dB2Wxopjvt3DY9CU3qMJc/8Ho6eoV8PWcUG/0M03avtMb1DYKQT63ZpYqPCUWL1"; # mkpasswd
+  };
+  path = ./.;
+}
