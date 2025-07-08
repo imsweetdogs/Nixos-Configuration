23348972b958ae3306f5430850bacdd2fd7bb437
imsweetdogs <git@sweetdogs.ru>
2025-07-08 16:27:43 +0000

    sysctl

--- config/modules/x86_64-linux/system/sysctl/default.nix
+++ config/modules/x86_64-linux/system/sysctl/default.nix
@@ -3,15 +3,11 @@
 with lib;
 
 let
-  # Базовые оптимизированные значения по-умолчанию.
   defaultSettings = {
-    # Улучшение производительности файловой системы
     "vm.swappiness" = "10";
     "vm.vfs_cache_pressure" = "50";
-    # Улучшение сетевой производительности
     "net.core.netdev_max_backlog" = "100000";
     "net.core.somaxconn" = "100000";
-    # Буферы для TCP
     "net.ipv4.tcp_rmem" = "4096 87380 16777216";
     "net.ipv4.tcp_wmem" = "4096 65536 16777216";
   };
