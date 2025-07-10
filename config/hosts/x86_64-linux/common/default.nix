89f5106f468d2dbe5a94c710f7dcdc266464ad02
imsweetdogs <git@sweetdogs.ru>
2025-07-10 18:32:43 +0000

    hotfix 2

--- config/hosts/x86_64-linux/common/default.nix
+++ config/hosts/x86_64-linux/common/default.nix
@@ -6,4 +6,4 @@
     (import "${flake.conf.path}/modules" { path = "${flake.conf.path}/modules/x86_64-linux"; lib = lib; })
     (import "${flake.conf.path}/modules" { path = ./modules; lib = lib; })
   ];
-}c
+}
