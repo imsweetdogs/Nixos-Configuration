a03f675bedf2d042f108e5e95c7d7bb221cc65b0
imsweetdogs <git@sweetdogs.ru>
2025-07-10 18:35:38 +0000

    hotfix

--- config/hosts/x86_64-linux/common/default.nix
+++ config/hosts/x86_64-linux/common/default.nix
@@ -1,9 +1,7 @@
-{ inputs, flake, modulesPath, lib, ... }: {
+{ inputs, flake, ... }: {
   hostname = "common";
   modules = [
     "${flake.conf.path}/overlays/nixpkgs"
-    (modulesPath + "/installer/scan/not-detected.nix")
-    (import "${flake.conf.path}/modules" { path = "${flake.conf.path}/modules/x86_64-linux"; lib = lib; })
-    (import "${flake.conf.path}/modules" { path = ./modules; lib = lib; })
+    ./system.nix
   ];
 }
--- config/hosts/x86_64-linux/common/system.nix
+++ config/hosts/x86_64-linux/common/system.nix
@@ -1,4 +1,9 @@
-{ inputs, flake, ... }: {
+{ inputs, flake, lib, modulesPath, ... }: {
+  imports = [
+    (import "${flake.conf.path}/modules" { path = "${flake.conf.path}/modules/x86_64-linux"; lib = lib; })
+    (import "${flake.conf.path}/modules" { path = ./modules; lib = lib; })
+    (modulesPath + "/installer/scan/not-detected.nix")
+  ];
   modules.hardware.storage.btrfs.enable = true;
 
   modules.network.nm.enable = true;
