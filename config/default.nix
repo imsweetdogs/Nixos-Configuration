113538673b9b4511059fe8f732f5f1d1d189a651
imsweetdogs <git@sweetdogs.ru>
2025-07-10 17:53:48 +0000

    delete modules

--- config/default.nix
+++ config/default.nix
@@ -1,11 +1,3 @@
 {
-  system = {
-    timeZone = "Europe/Moscow";
-    stateVersion = "25.05";
-    device = "/dev/nvme0n1";
-    users = [ "sweetdogs" ];
-    # lgbt = true;
-    baseHashedPassword = "$6$sweetdogs$tfWuwb0.wWlBGveduqu253BwOnoXmcDDH3fkWqvo0pLT5etrNXjyfG8ABIVSLyQSfR.nlnbrL3eC7LwXnAFcK1"; # mkpasswd
-  };
   path = ./.;
 }
